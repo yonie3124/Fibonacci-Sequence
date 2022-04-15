0d48f5a2a26f6d564a4330c9c4089cfa7c710b3c
Yonas <98472333+yonie3124@users.noreply.github.com>
2022-04-15 11:49:41 +0200

    commit

---
+++ main.js
@@ -0,0 +1,11 @@
+const fibonacci = (num = 1) => {
+    const series = [1, 1];
+    for (let i = 2; i < num; i++) {
+       const a = series[i - 1];
+       const b = series[i - 2];
+       series.push(a + b);
+    };
+    return series[num - 1];
+ };
+
+ console.log(fibonacci(10));
